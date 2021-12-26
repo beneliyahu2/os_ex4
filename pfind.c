49652165c523d54e6983690b637f259876e80bb7
beneliyahu2 <beneliyahu2@mail.tau.ac.il>
2021-12-26 19:45:29 +0200

    runs. the best I can ask at that poit

--- pfind.c
+++ pfind.c
@@ -5,18 +5,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <pthread.h>
+#include <stdatomic.h>
 
 //mutex
 pthread_mutex_t mutex;
-pthread_cond_t cv;
-pthread_cond_t not_empty;
-pthread_cond_t all_created;
-int sleeping_num = 0;
+//pthread_cond_t cv;
+//pthread_cond_t not_empty;
+//pthread_cond_t all_created;
+//int sleeping_num = 0;
 
 //------------------------------------------------------------------------------------------------
 // ------------------------------------- FIFO queue: ---------------------------------------------
@@ -40,6 +40,7 @@ fifo_queue *init_queue(){
 }
 
 int insert_to_queue(fifo_queue *q, char *rel_path){
+//    pthread_mutex_lock(&mutex); //lock
     // create new node:
     dir_node *new_node = malloc(sizeof(dir_node));
     if (!new_node){
@@ -49,7 +50,6 @@ int insert_to_queue(fifo_queue *q, char *rel_path){
     new_node->next = NULL;
 
     // insert it as head:
-    pthread_mutex_lock(&mutex); //lock
     if (q->head == NULL){
         q->head = new_node;
     }
@@ -58,14 +58,14 @@ int insert_to_queue(fifo_queue *q, char *rel_path){
     }
     q->tail = new_node;
     // if some searching threads are sleeping waiting for work, should wake up one of them:
-    pthread_cond_signal(&not_empty);
-    pthread_mutex_unlock(&mutex); //unlock
+//    pthread_cond_signal(&not_empty);
+//    pthread_mutex_unlock(&mutex); //unlock
     return 0;
 }
 
 dir_node *dequeue_head(fifo_queue *q){
+//    pthread_mutex_lock(&mutex); //lock
     dir_node *prev_head;
-    pthread_mutex_lock(&mutex); //lock
     if (q->head == NULL){
         return NULL;
     }
@@ -76,16 +76,17 @@ dir_node *dequeue_head(fifo_queue *q){
         return prev_head;
     }
     q->head = q->head->next;
-    pthread_mutex_unlock(&mutex); //unlock
     prev_head->next=NULL;
+//    pthread_mutex_unlock(&mutex); //unlock
     return prev_head;
 }
 
-// global variables:
+// ----------------------------------- global variables: --------------------------------------------------
 char *root_path;
 char *term;
 int threads_num;
 fifo_queue *q;
+atomic_int found_files_cnt = 0;
 
 // ------------------------------------ utilities: -----------------------------------------------------------
 
@@ -93,7 +94,7 @@ int is_dir(const char *path) {
     void *ret_val;
     struct stat stat_buf;
     if (stat(path, &stat_buf) != 0)
-        pthread_exit(ret_val); //todo - deal with the error and catch the exit in join (?)
+        pthread_exit(ret_val);
     return S_ISDIR(stat_buf.st_mode); //S_ISDIR macro returns non-zero if the file is a directory
 }
 // -----------------------------------------------------------------------------------------------------------
@@ -101,7 +102,6 @@ int is_dir(const char *path) {
 // -----------------------------------------------------------------------------------------------------------
 
 int search_in_dir(char *rel_dir_path){
-    int found_files_cnt = 0;
     struct dirent *entry = NULL;
     char entry_full_path[PATH_MAX];
     char entry_rel_path[PATH_MAX];
@@ -117,14 +117,15 @@ int search_in_dir(char *rel_dir_path){
     //scanning the entries of the directory:
     while((entry = readdir(dir)) != NULL){ //returns NULL when get to the end of the directory
         char *entry_name = entry->d_name;
+        printf("entry name: %s\n", entry_name); //todo del
         sprintf(entry_rel_path, "%s/%s", rel_dir_path, entry_name);
         sprintf(entry_full_path, "%s/%s", root_path, entry_rel_path);
+
 
         if (strcmp(entry_name, ".")==0 || strcmp(entry_name, "..")==0){
             // ignoring . and ..
         }
         else if (is_dir(entry_full_path) != 0){
-            errno = 0;
             entry_dir = opendir(entry_full_path);
             if (entry_dir == NULL) { // directory can’t be searched
                 printf("Directory %s: Permission denied.\n", entry_rel_path);
@@ -133,6 +134,7 @@ int search_in_dir(char *rel_dir_path){
                 //add to queue:
                 closedir(entry_dir);
                 insert_to_queue(q, entry_rel_path);
+                printf("after insert\n");
             }
         }
         else{ //regular file
@@ -143,47 +145,48 @@ int search_in_dir(char *rel_dir_path){
         }
     }
     closedir(dir);
-    printf("dir: '%s' have %d fit files\n\n", rel_dir_path, found_files_cnt); //todo del
 
-    return found_files_cnt;
+    return 0;
 }
 // --------------------------------------- thread_routine: --------------------------------------------
 
 void *thread_routine(){
 
+    pthread_mutex_lock(&mutex); //lock
+
     //Wait for all other searching threads to be created:
-    pthread_cond_wait(&all_created, &mutex);
+//    pthread_cond_wait(&all_created, &mutex);
 
     // Dequeue the head directory from the FIFO queue:
     dir_node *curr_node = dequeue_head(q);
     if (curr_node == NULL){ //empty queue
         //sleep (wait):
-        sleeping_num++;
-        pthread_cond_wait(&not_empty, &mutex);
-        if (sleeping_num == threads_num){
-            pthread_exit(0);
-        }
-        // todo - If all other searching threads are already waiting:
-            //todo - all searching threads should exit
-            //todo - return that the search in the tree is done
+//        sleeping_num++;
+//        pthread_cond_wait(&not_empty, &mutex);
+//        if (sleeping_num == threads_num){
+//            pthread_exit(0);
+//        }
+        pthread_exit(0);
+
+
     } else{
         // extract the directory relative path from the node:
         char curr_rel_path[PATH_MAX];
         strcpy(curr_rel_path, curr_node->rel_path);
 
         // search the directory of the node:
-        int num_of_finds_in_dir = search_in_dir(curr_rel_path);
+        search_in_dir(curr_rel_path);
+
+        pthread_mutex_unlock(&mutex); //unlock
 
         // return the search result:
-        pthread_exit((void*)num_of_finds_in_dir);
+        pthread_exit(NULL);
     }
 }
 
 // -------------------------------------------- search in tree: ----------------------------------------
 
 int search_in_tree(){
-
-    int num_of_finds_in_tree; // the returned value
 
     // Create a FIFO queue that holds directories:
     q = init_queue();
@@ -198,16 +201,14 @@ int search_in_tree(){
         pthread_create(&threads[i], NULL, thread_routine, NULL);
     }
     //wait for all threads to create, and then signals them to start searching:
-    pthread_cond_signal(&all_created);
+//    pthread_cond_signal(&all_created);
 
-    //wait for all threads to finish and collect all their return values (pthread_join):
-    int num_of_finds_in_dir;
+    //wait for all threads to finish (pthread_join):
     for (int i = 0; i < threads_num; i++){
-        pthread_join(threads[i], (void**)&num_of_finds_in_dir);
-        num_of_finds_in_tree += num_of_finds_in_dir;
+        pthread_join(threads[i], NULL);
     }
 
-    return num_of_finds_in_tree;
+    return 0;
 }
 
 // -------------------------------------------------------------------------------------------
@@ -221,12 +222,12 @@ int main(int argc, char *argv[]){
 
     // Initialize mutex and condition variable objects
     pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cv, NULL);
+//    pthread_cond_init(&cv, NULL);
 
     int res = search_in_tree();
-    printf("Done searching, found %d files\n", res);
+    printf("Done searching, found %d files\n", found_files_cnt);
 
     // destroy mutex and cv
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cv);
+//    pthread_cond_destroy(&cv);
 }
